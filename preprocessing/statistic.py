af29dca4bf4f9a90d49fc8a1fe60b8e84f7c7235
Xiang Jiang <xiangj1@Xiang-Surface.localdomain>
2020-10-08 17:43:47 -0700

    added statistic file for preprocessing

---
+++ preprocessing/statistic.py
@@ -0,0 +1,29 @@
+import json
+
+DATASET_PATH = 'dataset'
+MARKS_PATH = './preprocessing/marks.json'
+E2E_RESULT_PATH = './example_dataset.json'
+
+
+def reverseDict(dict):
+    return {v: k for k, v in dict.items()}
+
+
+def getStats():
+    stats = {}
+    marks = json.load(open(MARKS_PATH))
+    marks = reverseDict(marks)
+
+    result = json.load(open(E2E_RESULT_PATH))
+    for sentence in result.values():
+        for c in sentence:
+            if (c in marks):
+                stats[marks[c]] = stats.get(marks[c], 0) + 0.5
+
+    print(stats)
+    return
+
+
+if __name__ == "__main__":
+    getStats()
+    pass
